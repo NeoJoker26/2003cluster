1b75582629586e3fd26b5d954492e909be25a917
Jonathan Boxall <jonathan.boxall@students.plymouth.ac.uk>
2023-01-15 18:39:36 +0000

    Contact us popup

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,17 +1,30 @@
-﻿var modal = document.getElementById("aboutGoSModal");
-var btn = document.getElementById("aboutBtn");
-var span = document.getElementsByClassName("close")[0];
+﻿var modal_about = document.getElementById("aboutGoSModal");
+var modal_contact = document.getElementById("contactGoSModal");
+var btn_about = document.getElementById("aboutBtn");
+var btn_contact = document.getElementById("contactBtn");
+var about_close = document.getElementsByClassName("close-about")[0];
+var contact_close = document.getElementsByClassName("close-contact")[0];
 
-btn.onclick = function () {
-    modal.style.display = "block";
+
+btn_about.onclick = function () { //about us open
+        modal_about.style.display = "block"; 
+}
+btn_contact.onclick = function () { //contact us open BROKEn
+    modal_contact.style.display = "block";
 }
 
-span.onclick = function () {
-    modal.style.display = "none";
-}
 
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+about_close.onclick = function () { //about us close BROKEN
+    modal_about.style.display = "none";
+    
+}
+contact_close.onclick = function () { // contact us close BROKEN
+    modal_contact.style.display = "none";
+
+}
+window.onclick = function (event) { // add comment pls idk what this is
+    if (event.target == modal_about) {
+        modal_about.style.display = "none";
     }
+  
 }
