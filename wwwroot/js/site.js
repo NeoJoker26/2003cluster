86c5a9fe03aed4196a79d5495228b94fe50792f7
Kieran <kieran.kirkwood@students.plymouth.ac.uk>
2023-01-15 17:16:31 +0000

    modal popup

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,17 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var modal = document.getElementById("aboutGoSModal");
+var btn = document.getElementById("aboutBtn");
+var span = document.getElementsByClassName("close")[0];
 
-// Write your JavaScript code.
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
